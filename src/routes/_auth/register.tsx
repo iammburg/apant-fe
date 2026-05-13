a8ae33810d96486393d7130ef412038ab1d1dda1
iammburg <ilhamprabowo009@gmail.com>
2026-05-13 08:22:17 +0700

    feat: implement login and registration forms with routing

---
+++ src/routes/_auth/register.tsx
@@ -0,0 +1,14 @@
+import { RegisterForm } from '#/components/auth/register-form'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_auth/register')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  )
+}
